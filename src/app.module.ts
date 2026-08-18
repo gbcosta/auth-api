3d2f2263617f1fdc5d2321c16c6308b67d613180
Gabriel <gbcostasant@gmail.com>
2026-08-18 15:56:07 -0300

    setup docker and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [],
 })
